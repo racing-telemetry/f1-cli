09e6980e7f1b2d1b3050c269710f5854ca9fa46a
Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>
2022-04-10 16:18:39 +0300

    refactor: fix homebrew

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -3,5 +3,5 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-	go run main.go completion "$sh" >"completions/f1dump.$sh"
+	go run main.go completion "$sh" >"completions/f1.$sh"
 done
